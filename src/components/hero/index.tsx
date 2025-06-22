89aff078255bbe5a1d9c59401cbfba06d470add1
Abdelrahmen <Abdelrahmen@gmail.com>
2025-06-22 22:15:57 +0300

    init app, and build navbar and hero

---
+++ src/components/hero/index.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+
+function Hero() {
+  return (
+    <section className="flex flex-col h-[90vh] p-10 items-center justify-center text-white">
+      <h1 className="text-5xl text-center mt-10 py-10 font-extrabold bg-clip-text text-primary">
+        عبد الرحمن أحمد
+      </h1>
+
+      <h3
+        dir="rtl"
+        className="text-sm text-center md:text-xl leading-relaxed  bg-clip-text text-primary font-medium max-w-2xl mx-auto"
+      >
+        أنا مطور فول ستاك ويب متخصص في لغة
+        <span
+          dir="ltr"
+          className="inline-block font-semibold  mx-1 text-foreground "
+        >
+          JavaScript
+        </span>
+        أبني تطبيقات ويب متكاملة وعالية الأداء باستخدام
+        <span
+          dir="ltr"
+          className="inline-block font-semibold text-foreground mx-1"
+        >
+          Next.js
+        </span>
+        و
+        <span
+          dir="ltr"
+          className="inline-block font-semibold text-foreground mx-1"
+        >
+          NestJS
+        </span>
+        ، مع تركيز على كتابة كود نظيف، قابل للتوسع، وسهل الصيانة.
+      </h3>
+
+      <div className="bg-background/2 w-full h-full flex items-center justify-center">
+        <Image
+          src={"/svg/website-amico.svg"}
+          width={200}
+          height={200}
+          alt="svg-hero"
+          className="w-full h-full object-contain "
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+
+function Navbar() {
+  return (
+    <header className="sticky mb-12 bg-linear-to-r from-sky-700 to-cyan-600 top-0 z-50 backdrop-blur-md  border-b border-border">
+      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+        <h1 className="text-lg text-secondary font-bold">
+          <div className="flex items-center justify-center rounded-full">
+            <Image
+              src="/logo.jpg"
+              alt="Logo"
+              width={30}
+              height={30}
+              className="w-full h-full object-cover rounded-full"
+            />
+          </div>
+        </h1>
+        <nav className="hidden md:flex gap-6 text-sm font-medium">
+          <a href="#home" className="hover:underline hover:text-secondary">
+            الرئيسية
+          </a>
+          <a href="#projects" className="hover:underline hover:text-secondary">
+            المشاريع
+          </a>
+          <a href="#about" className="hover:underline hover:text-secondary ">
+            عني
+          </a>
+
+          <a href="#contact" className="hover:underline hover:text-secondary">
+            تواصل معي
+          </a>
+        </nav>
+        <div className="flex items-center gap-2"></div>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
